70d1c5b419360846954d22ac3b8c84ee37bb7274
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-07-13 13:27:24 +0000

    Deploying to gh-pages from @ TaurusVB/goit-react-hw-08-phonebook@e62bd462ae64b0691d69e6f4f33049d59ebeea80 🚀

---
+++ static/js/983.11f77eec.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[983],{3983:function(e,o,n){n.r(o);var h=n(184);o.default=function(){return(0,h.jsx)(h.Fragment,{children:(0,h.jsx)("h1",{children:"PhoneBook welcome page!"})})}}}]);
+//# sourceMappingURL=983.11f77eec.chunk.js.map
